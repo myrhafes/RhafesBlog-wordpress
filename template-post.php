6f5085fd312849e2953672f1651865e1216ec24d
Mohamed Yassine Rhafes <rhafes.medyassine@gmail.com>
2022-03-13 20:27:54 +0100

    Update template-post

--- template-post.php
+++ template-post.php
@@ -6,15 +6,17 @@
  */
 ?>
 <div class="right-content">
-    <?php if(have_posts()): while(have_posts()): the_post();?>
-        <h1><?php the_title();?></h1>
-        <p>
-            <img src="<?php the_post_thumbnail_url();?>" alt="" style="width:100%; height:auto;">
-        </p>
-        <?php the_content();?>
-    <?php get_footer();?>
-    <?php endwhile;
-    endif; ?>
+    <div class="post-content">
+        <?php if(have_posts()): while(have_posts()): the_post();?>
+            <h1 class="title"><?php the_title();?></h1>
+            <p class="image">
+                <img src="<?php the_post_thumbnail_url();?>" alt="">
+            </p>
+            <?php the_content();?>
+        <?php get_footer();?>
+        <?php endwhile;
+        endif; ?>
+    </div>
 </div>
 
 <?php get_footer();?>
